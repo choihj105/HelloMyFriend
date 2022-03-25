e882cd712364558728919fa3d9dab7beaabc0ffd
choihj105 <li0_0li2@naver.com>
2022-03-25 20:58:43 +0900

    feat: Add Player Animation

--- P_3D Action Game/Assets/Scripts/Player.cs
+++ P_3D Action Game/Assets/Scripts/Player.cs
@@ -7,13 +7,20 @@ public class Player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
-
+    bool wDown;
     Vector3 moveVec;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -22,12 +29,15 @@ public class Player : MonoBehaviour
         // 키보드입력에 따라 0 ~ 1로 변환 left right up down
         hAxis = Input.GetAxisRaw("Horizontal"); 
         vAxis = Input.GetAxisRaw("Vertical");
-
+        wDown = Input.GetButton("Run");
         // x y z
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         // transform
         transform.position += moveVec * speed * Time.deltaTime;
 
+        // animator
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+        anim.SetBool("isRun", wDown);
     }
 }
