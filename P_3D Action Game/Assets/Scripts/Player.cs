764bfe2ccf24adccb1c8e2aa62b9d30c08e22dc0
choihj105 <li0_0li2@naver.com>
2022-03-27 16:23:26 +0900

    feat: Add Jump Function

--- P_3D Action Game/Assets/Scripts/Player.cs
+++ P_3D Action Game/Assets/Scripts/Player.cs
@@ -7,8 +7,11 @@ public class Player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
-    bool wDown;
+    bool rDown;
+    bool jDown;
+    
     Vector3 moveVec;
+    Rigidbody rigid;
 
     Animator anim;
 
@@ -20,27 +23,54 @@ public class Player : MonoBehaviour
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+
+    void GetInput()
+    {
         // 키보드입력에 따라 0 ~ 1로 변환 left right up down
-        hAxis = Input.GetAxisRaw("Horizontal"); 
+        hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        wDown = Input.GetButton("Run");
+        rDown = Input.GetButton("Run");
+        jDown = Input.GetButtonDown("Jump");
+    }
+
+    void Move()
+    {
         // x y z
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         // transform
-        transform.position += moveVec * (wDown ? 1.3f : 1f) * speed * Time.deltaTime;
+        transform.position += moveVec * (rDown ? 1.3f : 1f) * speed * Time.deltaTime;
 
         // animator
         anim.SetBool("isWalk", moveVec != Vector3.zero);
-        anim.SetBool("isRun", wDown);
+        anim.SetBool("isRun", rDown);
+    }
 
+    void Turn()
+    {
         // Rotation
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if(jDown)
+        {
+            // 물리엔진에 힘을 준다. 여기선 즉발적인 Impulse
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+        }
     }
 }
