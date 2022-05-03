5a4afc9cd6f8fdf9a90ef07b699911fd47c09298
choihj105 <li0_0li2@naver.com>
2022-05-03 13:31:47 +0900

    error : 3D cemara

---
+++ P_3D Action Game/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed = 10f;
+    public float sensitivity = 100f;
+    public float clampAngle = 70f;
+
+    private float rotX;
+    private float rotY;
+
+    public Transform realCamera;
+    public Vector3 dirNormarized;
+    public Vector3 finalDir;
+    public float minDistance;
+    public float maxDistance;
+    public float finalDistance;
+    public float smoothness = 10f;
+    void Start()
+    {
+        rotX = transform.localRotation.eulerAngles.x;
+        rotY = transform.localRotation.eulerAngles.y;
+
+        dirNormarized = realCamera.localPosition.normalized;
+        finalDistance = realCamera.localPosition.magnitude;
+    }
+
+    void Update()
+    {
+        rotX += -(Input.GetAxis("Mouse Y")) * sensitivity * Time.deltaTime;    
+        rotY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion rot = Quaternion.Euler(rotX, rotY, 0);
+        transform.rotation = rot;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+
+        finalDir = transform.TransformPoint(dirNormarized * maxDistance);
+
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, finalDir, out hit))
+        {
+            finalDistance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+        }
+        else
+        {
+            finalDistance = maxDistance;
+        }
+
+        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition, dirNormarized * finalDistance, Time.deltaTime * smoothness);
+    }
+
+
+
+}
