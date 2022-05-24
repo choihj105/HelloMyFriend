fae198167d72e51abd6276978fff305e908297d1
choihj105 <li0_0li2@naver.com>
2022-05-24 09:51:33 +0900

    feat : Add Move

--- Photon_Sooter/Assets/Scripts/Player.cs
+++ Photon_Sooter/Assets/Scripts/Player.cs
@@ -14,6 +14,10 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
     public Text NicknameText;
     public Image HealthImage;
 
+    public int speed;
+    float axis;
+
+
     bool isGround;
     Vector3 curPos;
 
@@ -23,9 +27,41 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         NicknameText.color = PV.IsMine ? Color.green : Color.red;
     }
 
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            GetInput();
+
+            //이동
+            Move();
+        }
+    }
+
+    void GetInput()
+    {
+        axis = Input.GetAxisRaw("Horizontal");
+    }
+
+    void Move()
+    {
+        RB.velocity = new Vector2( speed * axis, RB.velocity.y);
+
+        if (axis != 0)
+        {
+            AN.SetBool("Walk", true);
+            PV.RPC("FlipXRPC", RpcTarget.AllBuffered, axis); // 재접속시 flipX를 동기화 해주기 위해 AllBuffered
+        }
+        else AN.SetBool("Walk", false);
+    }
+
+    [PunRPC]
+    void FlipXRPC(float axis) => SR.flipX = axis == -1;
+
+
     // 변수 동기화
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 }
